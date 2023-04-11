e38c449ab002561f643814623d6e98770ebb572b
OlexTS <sashokseven@gmail.com>
2023-04-11 22:46:09 +0300

    fixed err message

--- middlewares/postValidateBody.js
+++ middlewares/postValidateBody.js
@@ -3,15 +3,21 @@ const HttpError = require("../helpers/HttpError");
 const availableFields = (object) => {
   const objectFields = ["name", "email", "phone"];
   const fields = objectFields.filter((field) => object[field] === undefined);
-  return `missing required ${fields[0]} field`;
+  return fields.length > 1
+    ? `missing required ${fields.join(", ")} fields`
+    : `missing required ${fields[0]} field`;
 };
 
 const postValidateBody = (schema) => {
   const foo = (req, res, next) => {
     const { error } = schema.validate(req.body);
-    if (error) {
+    if (Object.keys(req.body).length < 3) {
       next(HttpError(400, availableFields(req.body)));
     }
+    if (error) {
+      next(HttpError(400, error.message));
+    }
+
     next();
   };
   return foo;
