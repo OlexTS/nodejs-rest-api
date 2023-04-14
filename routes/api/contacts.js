34a22ab51727e3f22926dcbc2fb80713ca09ee83
OlexTS <sashokseven@gmail.com>
2023-04-14 18:47:24 +0300

    updated operations

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,19 +1,35 @@
 const express = require("express");
-const { addSchema, changeSchema } = require("../../schemas/validateSchema");
+const { schemas } = require("../../models/contact");
 
 const controller = require("../../controllers/contacts");
-const { validateBody, postValidateBody } = require("../../middlewares");
+const {
+  validateBody,
+  postValidateBody,
+  isValidId,
+} = require("../../middlewares");
 
 const router = express.Router();
 
 router.get("/", controller.getContacts);
 
-router.get("/:contactId", controller.getContactById);
+router.get("/:contactId", isValidId, controller.getContactById);
 
-router.post("/", postValidateBody(addSchema), controller.addNewContact);
+router.post("/", postValidateBody(schemas.addSchema), controller.addNewContact);
 
 router.delete("/:contactId", controller.deleteContact);
 
-router.put("/:contactId", validateBody(changeSchema), controller.updateContact);
+router.put(
+  "/:contactId",
+  isValidId,
+  validateBody(schemas.changeSchema),
+  controller.updateContact
+);
+
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  controller.updateStatusContact
+);
 
 module.exports = router;
