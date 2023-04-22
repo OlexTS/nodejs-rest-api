f3de3d518643c710c1e4ac63d6b8658ab3afb38a
OlexTS <sashokseven@gmail.com>
2023-04-22 18:39:16 +0300

    added logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,6 +45,7 @@ const login = async (req, res) => {
   };
 
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  await User.findByIdAndUpdate(user._id, {token})
 
   res.json({
     token: token,
@@ -55,7 +56,14 @@ const login = async (req, res) => {
   });
 };
 
-const getCurrent = async (req, res, next) => {
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: '' })
+  
+  res.status(204).json({message: "No Content"})
+}
+
+const getCurrent = async (req, res) => {
   const { email, subscription } = req.user;
 
   res.json({ email, subscription });
@@ -64,5 +72,6 @@ const getCurrent = async (req, res, next) => {
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
+  logout: ctrlWrapper(logout),
   getCurrent: ctrlWrapper(getCurrent),
 };
